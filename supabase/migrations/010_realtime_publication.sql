78ede8c7e98ff397d3b503373c8b3b7d35811cd3
crzyNking <crzyNking@users.noreply.github.com>
2026-09-16 04:32:07 +0800

    fix: realtime migration catches undefined_table (skip missing tables gracefully)

--- supabase/migrations/010_realtime_publication.sql
+++ supabase/migrations/010_realtime_publication.sql
@@ -5,58 +5,68 @@ DO $$
 BEGIN
   ALTER PUBLICATION supabase_realtime ADD TABLE school_settings;
 EXCEPTION WHEN duplicate_object THEN NULL;
+  WHEN undefined_table THEN NULL;
 END $$;
 
 DO $$
 BEGIN
   ALTER PUBLICATION supabase_realtime ADD TABLE website_settings;
 EXCEPTION WHEN duplicate_object THEN NULL;
+  WHEN undefined_table THEN NULL;
 END $$;
 
 DO $$
 BEGIN
   ALTER PUBLICATION supabase_realtime ADD TABLE homepage_content;
 EXCEPTION WHEN duplicate_object THEN NULL;
+  WHEN undefined_table THEN NULL;
 END $$;
 
 DO $$
 BEGIN
   ALTER PUBLICATION supabase_realtime ADD TABLE announcements;
 EXCEPTION WHEN duplicate_object THEN NULL;
+  WHEN undefined_table THEN NULL;
 END $$;
 
 DO $$
 BEGIN
   ALTER PUBLICATION supabase_realtime ADD TABLE news;
 EXCEPTION WHEN duplicate_object THEN NULL;
+  WHEN undefined_table THEN NULL;
 END $$;
 
 DO $$
 BEGIN
   ALTER PUBLICATION supabase_realtime ADD TABLE events;
 EXCEPTION WHEN duplicate_object THEN NULL;
+  WHEN undefined_table THEN NULL;
 END $$;
 
 DO $$
 BEGIN
   ALTER PUBLICATION supabase_realtime ADD TABLE programs;
 EXCEPTION WHEN duplicate_object THEN NULL;
+  WHEN undefined_table THEN NULL;
 END $$;
 
 DO $$
 BEGIN
   ALTER PUBLICATION supabase_realtime ADD TABLE services;
 EXCEPTION WHEN duplicate_object THEN NULL;
+  WHEN undefined_table THEN NULL;
 END $$;
 
 DO $$
 BEGIN
   ALTER PUBLICATION supabase_realtime ADD TABLE gallery;
 EXCEPTION WHEN duplicate_object THEN NULL;
+  WHEN undefined_table THEN NULL;
 END $$;
 
 DO $$
 BEGIN
   ALTER PUBLICATION supabase_realtime ADD TABLE enrollment_submissions;
 EXCEPTION WHEN duplicate_object THEN NULL;
+  WHEN undefined_table THEN NULL;
 END $$;
